82de5ff8468dccdf2df542267240c4f00086aae0
Harmikl <Harmikl57@gmail.com>
2021-05-07 21:46:10 +0300

    Added_methods

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -21,6 +25,7 @@ public class FirstTest {
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
         capabilities.setCapability("app","/Users/maksimkharmak/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        capabilities.setCapability("udid","1c88f784220d7ece");
         driver= new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
     }
     @After
@@ -31,6 +36,47 @@ public class FirstTest {
     @Test
     public void firstTest()
     {
-        System.out.println("First test run");
+        waitForElementByXpathAndClick( // два слеша означают любую вложенность элементов, * то есть любой элемент, [contains] выражение позволяющее искать по частичному совпадению
+                "//*[contains(@text,'Search Wikipedia')]",
+                "Cannot find 'Search Wikipedia'",
+                5
+        );
+        waitForElementByXpathAndSendKeys(
+                "//*[contains(@text,'Search…')]",
+                "Java",
+                "Cannot find 'Search…'",
+                5
+        );
+        waitForElementPresentByXpath(
+                "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']",
+                "Cannot find 'Object-oriented programming language' topic searching by Java",
+                15
+        );
+    }
+
+
+
+    private WebElement waitForElementPresentByXpath(String xpath,String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message+ "\n");
+        By by= By.xpath(xpath);
+        return  wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+    private WebElement waitForElementPresentByXpath(String xpath,String error_message) {
+        return waitForElementPresentByXpath(xpath, error_message,5);
+    }
+    private WebElement waitForElementByXpathAndClick(String xpath, String error_message, long timeoutInSeconds){//написали метод который сначала дожидается какого-то
+        //элемента которыый передается в xpath и затем происходит клик
+        WebElement element= waitForElementPresentByXpath(xpath, error_message,timeoutInSeconds);
+        element.click();
+        return element;
+    }
+    private WebElement waitForElementByXpathAndSendKeys(String xpath, String value, String error_message, long timeoutInSeconds){//написали метод который сначала дожидается какого-то
+        //элемента которыый передается в xpath и затем происходит клик
+        WebElement element= waitForElementPresentByXpath(xpath, error_message,timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
     }
 }
