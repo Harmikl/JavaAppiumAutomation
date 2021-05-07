d28eb949324c7dd012e02614adbf8cbc31111a6a
Harmikl <Harmikl57@gmail.com>
2021-05-07 22:36:09 +0300

    Just one more commit)

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -13,29 +13,29 @@ import java.net.URL;
 
 public class FirstTest {
     private AppiumDriver driver;
+
     @Before
 
-    public void setUp() throws Exception
-    {
-        DesiredCapabilities capabilities= new DesiredCapabilities();
-        capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("deviceName","AndroidTestDevice");
-        capabilities.setCapability("platformVersion","8.0");
-        capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("appPackage","org.wikipedia");
-        capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","/Users/maksimkharmak/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-        capabilities.setCapability("udid","1c88f784220d7ece");
-        driver= new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "/Users/maksimkharmak/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        capabilities.setCapability("udid", "1c88f784220d7ece");
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
+
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
         driver.quit();
     }
+
     @Test
-    public void firstTest()
-    {
+    public void firstTest() {
         waitForElementByXpathAndClick( // два слеша означают любую вложенность элементов, * то есть любой элемент, [contains] выражение позволяющее искать по частичному совпадению
                 "//*[contains(@text,'Search Wikipedia')]",
                 "Cannot find 'Search Wikipedia'",
@@ -54,29 +54,74 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void testCancelSearch() {
+        waitForElementByIdhAndClick(
+                "org.wikipedia:id/search_container",
+                "Cannot find 'Search Wikipedia'",
+                5
+        );
+        waitForElementByIdhAndClick(
+                "org.wikipedia:id/search_close_btn",
+                "Cannot find close icon",
+                5
+        );
+        waitForElementNotPresent(
+                "org.wikipedia:id/search_close_btn",
+                "X is still on the page",
+                5
+        );
+    }
 
 
-    private WebElement waitForElementPresentByXpath(String xpath,String error_message, long timeoutInSeconds) {
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
-        wait.withMessage(error_message+ "\n");
-        By by= By.xpath(xpath);
-        return  wait.until(
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
                 ExpectedConditions.presenceOfElementLocated(by)
         );
     }
-    private WebElement waitForElementPresentByXpath(String xpath,String error_message) {
-        return waitForElementPresentByXpath(xpath, error_message,5);
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message) {
+        return waitForElementPresentByXpath(xpath, error_message, 5);
     }
-    private WebElement waitForElementByXpathAndClick(String xpath, String error_message, long timeoutInSeconds){//написали метод который сначала дожидается какого-то
+
+    private WebElement waitForElementByXpathAndClick(String xpath, String error_message, long timeoutInSeconds) {//написали метод который сначала дожидается какого-то
         //элемента которыый передается в xpath и затем происходит клик
-        WebElement element= waitForElementPresentByXpath(xpath, error_message,timeoutInSeconds);
+        WebElement element = waitForElementPresentByXpath(xpath, error_message, timeoutInSeconds);
         element.click();
         return element;
     }
-    private WebElement waitForElementByXpathAndSendKeys(String xpath, String value, String error_message, long timeoutInSeconds){//написали метод который сначала дожидается какого-то
-        //элемента которыый передается в xpath и затем происходит клик
-        WebElement element= waitForElementPresentByXpath(xpath, error_message,timeoutInSeconds);
+
+    private WebElement waitForElementByXpathAndSendKeys(String xpath, String value, String error_message, long timeoutInSeconds) {//написали метод который сначала дожидается какого-то
+        //элемента которыый передается в xpath и затем вводит значение
+        WebElement element = waitForElementPresentByXpath(xpath, error_message, timeoutInSeconds);
         element.sendKeys(value);
         return element;
+    }
+
+    private WebElement waitForElementPresentById(String id, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.id(id);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementByIdhAndClick(String id, String error_message, long timeoutInSeconds) {//написали метод который сначала дожидается какого-то
+        //элемента которыый передается в id и затем происходит клик
+        WebElement element = waitForElementPresentById(id, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+    private boolean waitForElementNotPresent(String id, String error_message, long timeoutInSeconds){//метод проверяет что элемента нету
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.id(id);
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
     }
 }
