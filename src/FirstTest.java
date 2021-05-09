2e87df2a485bad31bdae94155142a46b79481f9d
Harmikl <Harmikl57@gmail.com>
2021-05-09 16:53:09 +0300

    Ex4*: Тест: проверка слов в поиске

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
 
 public class FirstTest {
     private AppiumDriver driver;
@@ -28,6 +29,7 @@ public class FirstTest {
         capabilities.setCapability("app", "/Users/maksimkharmak/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
         capabilities.setCapability("udid", "1c88f784220d7ece");
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
     }
 
     @After
@@ -49,43 +51,14 @@ public class FirstTest {
                 5
         );
         waitForElementPresent(
-                By.id("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[contains(@text,'Object-oriented programming language')]"),
                 "Cannot find 'Object-oriented programming language' topic searching by Java",
                 15
         );
     }
 
     @Test
-    public void testCancelSearch() {
-        waitForElementAndClick(
-                By.id("org.wikipedia:id/search_container"),
-                "Cannot find 'Search Wikipedia'",
-                5
-        );
-        waitForElementAndSendKeys(
-                By.xpath("//*[contains(@text,'Search…')]"),
-                "Java",
-                "Cannot find 'Search…'",
-                5
-        );
-        waitForElementAndClear(
-                By.id("org.wikipedia:id/search_src_text"),
-                "Cannot clear 'Java'",
-                5
-        );
-        waitForElementAndClick(
-                By.id("org.wikipedia:id/search_close_btn"),
-                "Cannot find close icon",
-                5
-        );
-        waitForElementNotPresent(
-                By.id("org.wikipedia:id/search_close_btn"),
-                "X is still on the page",
-                5
-        );
-    }
-    @Test
-    public void testCompareArticleTitle(){
+    public void testCompareArticleTitle() {
         waitForElementAndClick( // два слеша означают любую вложенность элементов, * то есть любой элемент, [contains] выражение позволяющее искать по частичному совпадению
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find 'Search Wikipedia'",
@@ -97,7 +70,7 @@ public class FirstTest {
                 "Cannot find 'Search…'",
                 5
         );
-        waitForElementAndClick( // два слеша означают любую вложенность элементов, * то есть любой элемент, [contains] выражение позволяющее искать по частичному совпадению
+        waitForElementAndClick(
                 By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
                 "Cannot find 'Object-oriented programming language' topic searching by Java",
                 5
@@ -107,54 +80,89 @@ public class FirstTest {
                 "Cannot find 'Java (programming language)'",
                 10
         );
-        String article_title= title_element.getAttribute("text");
+        String article_title = title_element.getAttribute("text");
         Assert.assertEquals(
-                "We see unexpected title" ,
-                "Java (programming language)" ,
+                "We see unexpected title",
+                "Java (programming language)",
                 article_title
         );
 
     }
- @Test
- public void cancelSearch(){
-     waitForElementAndClick(
-             By.xpath("//*[contains(@text,'Search Wikipedia')]"),
-             "Cannot find 'Search Wikipedia'",
-             5
-     );
-     waitForElementAndSendKeys(
-             By.xpath("//*[contains(@text,'Search…')]"),
-             "Weather",
-             "Cannot find 'Search…'",
-             10
-     );
-     waitForElementPresent(//1st search result
-             By.xpath("//*[contains(@text,'State of the atmosphere')]"),
-             "Cannot find 'State of the atmosphere'",
-             5
-     );
-     waitForElementPresent(//2nd search result
-             By.xpath("//*[contains(@text,'American radical organization')]"),
-             "Cannot find 'American radical organization'",
-             5
-     );
-     waitForElementAndClear(
-             By.id("org.wikipedia:id/search_src_text"),
-             "Cannot clear 'Weather'",
-             5
-     );
-     waitForElementNotPresent(
-             By.xpath("//*[contains(@text,'State of the atmosphere')]"),
-             "'State of the atmosphere' is still here",
-             5
-     );
-     waitForElementNotPresent(
-             By.xpath("//*[contains(@text,'American radical organization')]"),
-             "'American radical organization' is still here",
-             5
-     );
 
- }
+    @Test
+    public void cancelSearch() {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia'",
+                5
+        );
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Weather",
+                "Cannot find 'Search…'",
+                10
+        );
+        waitForElementPresent(//1st search result
+                By.xpath("//*[contains(@text,'State of the atmosphere')]"),
+                "Cannot find 'State of the atmosphere'",
+                5
+        );
+        waitForElementPresent(//2nd search result
+                By.xpath("//*[contains(@text,'American radical organization')]"),
+                "Cannot find 'American radical organization'",
+                5
+        );
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot clear 'Weather'",
+                5
+        );
+        waitForElementNotPresent(
+                By.xpath("//*[contains(@text,'State of the atmosphere')]"),
+                "'State of the atmosphere' is still here",
+                5
+        );
+        waitForElementNotPresent(
+                By.xpath("//*[contains(@text,'American radical organization')]"),
+                "'American radical organization' is still here",
+                5
+        );
+    }
+
+    @Test
+    public void checkJavaWordInSearchResults() {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia'",
+                5
+        );
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "JAVA",
+                "Cannot find 'Search…'",
+                10
+        );
+        waitForElementPresent(
+                By.id("org.wikipedia:id/page_list_item_container"),
+                " Cannot find item containers '",
+                5
+        );
+        List<WebElement> listOfElementsWithItemContainers =  driver.findElements(By.id("org.wikipedia:id/page_list_item_container")); //находим количество контейнеров
+
+        waitForElementPresent(
+                By.xpath("//*[contains(@text,'Java')]"),
+                "Cannot find 'Java '",
+                5
+        );
+        List<WebElement> listOfElementsWithJava =  driver.findElements(By.xpath("//*[contains(@text,'Java')]")); //находим количество xpath с текстом 'Java'
+
+        Assert.assertEquals(
+                "List of containers is not equal to list of xpathes with 'Java' text",
+                listOfElementsWithItemContainers.size(),
+                listOfElementsWithJava.size()
+        );
+    }
+
 
 
 
@@ -171,7 +179,7 @@ public class FirstTest {
     }
 
     private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds) {//написали метод который сначала дожидается какого-то
-        //элемента которыый передается в xpath и затем происходит клик
+        //элемента которыый передается в xpath or id и затем происходит клик
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
         element.click();
         return element;
@@ -184,14 +192,15 @@ public class FirstTest {
         return element;
     }
 
-    private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds){//метод проверяет что элемента нету
+    private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds) {//метод проверяет что элемента нету
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
         return wait.until(
                 ExpectedConditions.invisibilityOfElementLocated(by)
         );
     }
-    private WebElement waitForElementAndClear (By by, String error_message, long timeoutInSeconds){
+
+    private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds) {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
         element.clear();
         return element;
