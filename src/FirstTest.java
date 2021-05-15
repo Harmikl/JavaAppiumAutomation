017715381e1471bc98e936be61a1b51c3a56ad72
MaxHar <Harmikl57@gmail.com>
2021-05-15 19:10:52 +0300

    Just commit

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,10 +1,12 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -88,7 +90,32 @@ public class FirstTest {
         );
 
     }
+    @Test
+    public void testSwipeArticle() {
+        waitForElementAndClick( // два слеша означают любую вложенность элементов, * то есть любой элемент, [contains] выражение позволяющее искать по частичному совпадению
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia'",
+                5
+        );
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Appium",
+                "Cannot find 'Search…'",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[contains(@text,'Appium')]"),
+                "Cannot find 'Appium'",
+                5
+        );
+        swipeUpToFindElement(
+                By.xpath("//*[contains(@text,'View page in browser')]"),
+                "Cannot find the end of the article",
+                5
+        );
 
+
+    }
     @Test
     public void cancelSearch() {
         waitForElementAndClick(
@@ -162,6 +189,125 @@ public class FirstTest {
                 listOfElementsWithJava.size()
         );
     }
+    @Test
+    public void saveFirstArticleToMyList(){
+        waitForElementAndClick( // два слеша означают любую вложенность элементов, * то есть любой элемент, [contains] выражение позволяющее искать по частичному совпадению
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia'",
+                5
+        );
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find 'Search…'",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Object-oriented programming language' topic searching by Java",
+                5
+        );
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find 'Java (programming language)'",
+                10
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),//класс в котором будем искать кнопку и в нем @content-desc
+                "Cannot find 'More options'",
+                10
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to reading list']"),
+                "Cannot find 'Add to reading list'",
+                10
+        );
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find 'Got it'",
+                5
+        );
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input field to set name of article",
+                5
+        );
+        String name_of_folder = "Learning programming";
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                name_of_folder,
+                "Cannot find put 'Learning programming' in input field",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='OK']"),
+                "Cannot find 'OK' button ",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot tap 'X' icon",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Cannot find 'My lists' icon",
+                5
+        );
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/item_title"),
+                "Cannot find 'Learning programming' in my lists",
+                5
+        );
+        waitForElementPresent(
+             By.xpath("//*[@text='"+ name_of_folder + "' ]"),
+                "Cannot find 'Learning programming' in my lists",
+                5
+        );
+        swipeElementToLeft(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "cannot find Java (programming language) in My lists"
+        );
+        waitForElementNotPresent(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Still Java (programming language) in My lists",
+                5
+        );
+
+    }
+    @Test
+    public void testAmountOfNotEmptySearch (){
+        String search_line= "Linkin Park Diskography";
+        String search_result_container ="//*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']";
+        //  /* означает что мы спускаемся на один уровень ниже
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia'",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                search_line,
+                "Cannot find 'Search line'",
+                5
+        );
+
+        waitForElementPresent(
+                By.xpath(search_result_container),
+                "Cannot find anything by request " + search_line ,
+                15
+        );
+        int amount_of_search_result = getAmountOfElement(
+                By.xpath(search_result_container)
+        );
+        Assert.assertTrue(
+                "We found few results",
+                amount_of_search_result>0
+
+        );
+    }
 
 
 
@@ -204,5 +350,57 @@ public class FirstTest {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
         element.clear();
         return element;
+    }
+    protected void swipeUp(int timeOfSwipe){
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int x  = size.width/2;
+        int start_y = (int) (size.height*0.8); //приводим к int потому что хз будет double или int
+        int end_y = (int) (size.height*0.2);
+        action
+                .press(x,start_y)
+                .waitAction(timeOfSwipe)
+                .moveTo(x,end_y)
+                .release()
+                .perform();// тут мы написали что нужно нажать, сместить, потом опять нажать и все это запустить
+
+    }
+    protected void swipeUpQuick(){
+        swipeUp(200);
+    }
+    protected void swipeUpToFindElement(By by, String error_message, int max_swipes){
+        int already_swiped=0;
+        while (driver.findElements(by).size()==0){//эта функция находит все элементы
+
+            if(already_swiped > max_swipes){
+                waitForElementPresent(by,"Cann not find element by swiping up\n"+error_message,0);
+                return;
+            }
+            swipeUpQuick();
+            ++already_swiped;
+        }
+
+    }
+    protected void swipeElementToLeft(By by, String error_message){
+        WebElement element = waitForElementPresent(by,error_message,10);
+        int left_x = element.getLocation().getX(); //это запишет самую левую координату п оси x
+        int right_x = left_x + element.getSize().getWidth();//это запишет самую правую координату п оси x
+        int upper_y = element.getLocation().getY();
+        int lower_y = upper_y + element.getSize().getHeight();
+        int middle_y = (upper_y+lower_y)/2;
+
+        TouchAction action = new TouchAction(driver);
+        action
+                .press(right_x,middle_y)
+                .waitAction(300)
+                .moveTo(left_x,middle_y)
+                .release()
+                .perform();
+    }
+    private int getAmountOfElement(By by){
+        List elements = driver.findElements(by);
+        // List - фуекция которая создает некий список, element -  название переменной,
+        // то есть мы возвращаем количество элементов, которые нашли при помощи  driver.findElements(by)
+        return elements.size();
     }
 }
